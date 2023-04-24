424f6ebeae9a7e6a78aafe6e5e5c0d99aa6fae99
diegodusan <128428190+diegodusan@users.noreply.github.com>
2023-04-23 21:42:47 -0500

    Initial commit

---
+++ app/src/main/java/com/diego/androidappprueba/PantallaDeCarga.java
@@ -0,0 +1,14 @@
+package com.diego.androidappprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaDeCarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_de_carga);
+    }
+}
